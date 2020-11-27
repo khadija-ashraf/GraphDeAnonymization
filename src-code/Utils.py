6dfb7c535bab0d2a5c39cc63cfa7132a440a4167
Khadija <khadija.csedu@gmail.com>
2020-11-27 16:18:08 -0500

    Computing Similarity Score for every lnode

--- src-code/Utils.py
+++ src-code/Utils.py
@@ -1,11 +1,12 @@
 import math
+import statistics
 
 class Utils:
 
-    def __init__(self):
-        self.lgraph = []  #left graph, typically the target graph, here its G1
-        self.rgraph = []  #right graph, typically the auxiliary graph, here its G2
-        self.mapping = False  # given seed nodes
+    def __init__(self, lgraph = None, rgraph = None, mapping = None):
+        self.lgraph = lgraph  #left graph, typically the target graph, here its G1
+        self.rgraph = rgraph  #right graph, typically the auxiliary graph, here its G2
+        self.mapping = mapping  # given seed node pairs
 
     @staticmethod
     def match_score(lgraph, rgraph, mapping, lnode):
@@ -18,3 +19,39 @@ class Utils:
                 scores[rnode] += 1 / math.sqrt(rnode.in_degree)
         return scores
 
+    def get_matched_neigbr_count(self, lnode, rnode):
+        matched_neighbr_count = 0
+        lnode_neighbrs = self.lgraph.adj[lnode] # fetching neighbors of lnode from lgraph
+        rnode_neighbrs = self.rgraph.adj[rnode] # fetching neighbors of rnode from rgraph
+        for lnode_neighbr in lnode_neighbrs:
+            if lnode_neighbr not in self.mapping: continue
+            mapped_rseed_node = self.mapping[lnode_neighbr]
+            if mapped_rseed_node and list(mapped_rseed_node)[0] in rnode_neighbrs: continue
+            matched_neighbr_count = matched_neighbr_count + 1
+
+        return matched_neighbr_count
+
+    @staticmethod
+    def eccentricity(lnode_scores):
+
+        if len(lnode_scores) < 2:
+            print(" Invalid Input ")
+            return 0
+        arr_size = len(lnode_scores)
+        largest = second = -2454635434
+
+        # Find the largest element
+        #for i in range(0, arr_size):
+        largest = max(lnode_scores)
+
+        # Find the second largest element
+        for i in range(0, arr_size):
+            if lnode_scores[i] != largest:
+                second = max(second, lnode_scores[i])
+
+        standard_dev = statistics.stdev(lnode_scores)
+
+        print("Standard Deviation: ", standard_dev)
+        if second == -2454635434 or (largest - second) == 0 or standard_dev == 0:
+            return 0
+        return (largest - second) / statistics.stdev(lnode_scores)
--- src-code/seed_based_de_anony.py
+++ src-code/seed_based_de_anony.py
@@ -1,8 +1,10 @@
 import networkx as nx
-import numpy as np
-
-import matplotlib.pyplot as plt
 from Utils import *
+import numpy as np
+import time
+
+THETA = 1.0
+START_TIME = time.time()
 
 G1 = nx.read_edgelist('input/G1.edgelist', comments='#', delimiter = ' ',
                       create_using=nx.DiGraph(), nodetype=int)
@@ -25,16 +27,38 @@ seeds = nx.read_edgelist('input/seed_node_pairs.txt', comments='#', delimiter = 
 lgraph = G1
 rgraph = G2
 
+utils = Utils(lgraph, rgraph, seeds)
+
+
 # Propagation:
-# Input: lgraph, rgraph
+# Input: lgraph: left graph, rgraph: right graph
 # Note: we are implementing the propagationStep including all the heuristics
 # (Edge directionality, Node degrees, Revisiting nodes, Reverse match)
 # mentioned by the authors of the paper "De-anonymizing Social Networks by Arvind N. and Vitaly S."
 
+# Revisiting nodes: Following the authors approach we are iterating for all nodes
+# in lgraph to find best mapping among all rgraph nodes.
 for lnode in lgraph.nodes:
-    scores = [0 for rnode in rgraph.nodes]
+    similarity_scores = dict()
+    for rnode in rgraph.nodes:
+        similarity_scores[rnode] = utils.get_matched_neigbr_count(lnode, rnode)
+    max_score = max(similarity_scores.values())  # maximum score
+    max_scoring_nodes = [k for k, v in similarity_scores.items() if v == max_score]  # getting all keys containing the `maximum`
 
-    scores[lnode] = Utils.match_score(lgraph, rgraph, seeds, lnode)
+    print('%d is max score, and %d number of nodes scored max ' %(max_score,len(max_scoring_nodes)))
+    picked_max_score_rnode = max_scoring_nodes[0] # the first node
+    print("Picked max rnode: ", picked_max_score_rnode)
     break
+'''
+    if Utils.eccentricity(scores[lnode]) < THETA: continue
+    # pick node from rgraph.nodes where  scores[lnode][node] = max(scores[lnode])
+    #new_mapping_rnode = max(range(len(scores[0])), key=scores[0].__getitem__)
+    new_mapping_rnode = scores[0].argmax()
 
+    print("new mapping lnode : ", lnode)
+    print("new mapping rnode : ", new_mapping_rnode)
+    break
+#seeds.add_edge(lnode, new_mapping_rnode)
+'''
 
+print("--- Total Execution time : %s seconds ---" % (time.time() - START_TIME))
